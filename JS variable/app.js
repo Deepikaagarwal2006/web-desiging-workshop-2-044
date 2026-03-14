12b64cb78a84c14c617453f43a0a34417783683a
Deepikaagarwal2006 <agarwaldeepika2006@gmail.com>
2026-03-14 16:23:40 +0530

    var.js

---
+++ JS variable/app.js
@@ -0,0 +1,21 @@
+console.log("Hello how are you ECE 2");
+
+// Show message in console
+console.log("hello ECE 2 welcome to javascript");
+
+// Show on alert popup
+alert("welcome to javascript!");
+
+// Change the page content background color
+document.body.style.backgroundColor = "pink";
+
+// Example: Add event listener to a button with id 'button'
+const button = document.getElementById('button');
+if (button) {
+    button.addEventListener('click', function() {
+        alert('Button clicked!');
+    });
+}
+
+
+
