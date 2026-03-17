f5d627b1fe5fc5b6372427dea2cdc232d42a8c66
Deepikaagarwal2006 <agarwaldeepika2006@gmail.com>
2026-03-17 11:28:40 +0530

    var.js

--- JS variable/var.js
+++ JS variable/var.js
@@ -1,4 +1,35 @@
-var name = "Deepika Mittal";
-var age = 19;
-console.log(name);
-console.log(age);
+// var name = "Deepika Mittal";
+// var age = 19;
+// console.log(name);
+// console.log(age);
+
+// dynamic type language 
+let name = "Deepika Mittal";
+let age = 19;
+let city = "Delhi";
+console.log("Name: " + name);
+console.log("Age: " + age); 
+console.log("City: " + city);    
+
+// operators 
+let total = 70 + 50+ 30; // additon 
+let average = total / 3; // division
+console.log("Total: " + total);
+console.log("Average: " + average);
+
+// loops 
+// intialization condition and increment/decreament 
+for (let i = 1; i <= 5; i++) {
+    console.log("Subject Number: " + i);
+}
+
+average = 85;
+if (average >= 90) {
+    console.log("Grade: A");     
+} else if (average >= 80) {
+    console.log("Grade: B");
+} else if (average >= 70) {
+    console.log("Grade: C");
+} else {
+    console.log("Grade: F");
+}
